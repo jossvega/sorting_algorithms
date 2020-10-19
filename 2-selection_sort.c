2730e0c644fe33a38c6fad177c80f3166c3dcc2f
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-10-19 22:30:12 +0000

    Adding if array == NULL or empty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,6 +9,9 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min, swap;
 
+	if (!array || size <= 1)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		/*set current element as minimun */
